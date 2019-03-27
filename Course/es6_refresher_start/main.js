57f00947552ed258432be11c1f913473682ca26e
corey <cacarter08@gmail.com>
2019-03-26 20:43:12 -0400

    es6-refresher-part1

--- Course/es6_refresher_start/main.js
+++ Course/es6_refresher_start/main.js
@@ -1,12 +1,57 @@
 // CONST & LET
+const namess = 'brad';
+let names = 'john';
+
+name = 'brad';
+
+console.log(name);
+
+const candybar = {
+  flavor: 'chocolate',
+  length: 6
+};
+
+const nums = [1, 2, 3, 5];
+console.log(nums);
 
 // ARROW FUNCTIONS
+function sayHello() {
+  console.log('Hello');
+}
+
+sayHello();
+
+const whatShouldISay = name => console.log(`my name is ${name}`);
+
+whatShouldISay('babcock');
+
+const sayHey = () => console.log('hey');
 
 // FOREACH
+const fruits = ['Apples', 'Oranges', 'Bananas'];
+
+fruits.forEach((fruit, index) => {
+  console.log(index + 1 + ' ' + fruit);
+});
 
 // MAP
+const singleFruit = fruits.map(fruit => fruit.slice(0, -1).toUpperCase());
+
+console.log(singleFruit);
 
 // FILTER
+
+const people = [
+  { id: 1, name: 'karen' },
+  { id: 2, name: 'bob' },
+  { id: 3, name: 'corey' }
+];
+
+const filterPeople = people.filter(people => people.id !== 2);
+console.log(filterPeople);
+
+const filterName = people.filter(people => people.name == 'corey');
+console.log(filterName);
 
 // SPREAD
 
