beb23c37e51d1db75c5214d848f72a25d7bd0ed6
corey <cacarter08@gmail.com>
2019-03-27 21:40:40 -0400

    updated

--- contactmanager/src/components/Contacts.js
+++ contactmanager/src/components/Contacts.js
@@ -1,26 +1,26 @@
-import React, { Component } from "react";
-import Contact from "./Contact";
+import React, { Component } from 'react';
+import Contact from './Contact';
 
 class Contacts extends Component {
   state = {
     contacts: [
       {
         id: 1,
-        name: "John Doe",
-        email: "jdoe@gmail.com",
-        phone: "111-222-3333"
+        name: 'John Doe',
+        email: 'jdoe@gmail.com',
+        phone: '111-222-3333'
       },
       {
         id: 2,
-        name: "Yizel Doe",
-        email: "ydoe@gmail.com",
-        phone: "111-112-3333"
+        name: 'Yizel Doe',
+        email: 'ydoe@gmail.com',
+        phone: '111-112-3333'
       },
       {
         id: 3,
-        name: "Brandon Doe",
-        email: "bdoe@gmail.com",
-        phone: "111-222-3111"
+        name: 'Brandon Doe',
+        email: 'bdoe@gmail.com',
+        phone: '111-222-3111'
       }
     ]
   };
